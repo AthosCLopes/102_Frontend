ad09ff3262a3192b743fe21d2f230fd31a0d2caf
ATHOS CHAVES LOPES DE ALMEIDA <07072193022@senacrs.edu.br>
2025-10-03 12:15:26 -0300

    03/10/2025

---
+++ javascript/exemplo3.js
@@ -0,0 +1,17 @@
+let numeros  = [1, 2, 3];
+
+//Mostrar e adicionar no FIM
+console.log(numeros);
+numeros.push(6)
+console.log(numeros);
+
+//Mostrar e reomver no INICIO
+numeros.shift(1);
+console.log(numeros);
+
+//Mostrar e adicionaro no INICIO
+numeros.unshift(-3)
+console.log(numeros);
+
+//Mostrar um numero especifico
+console.log(numeros[4])
